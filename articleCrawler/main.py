f6fdcdbbf39df71a30e4cc007e8ef8543ca08ff6
SavorTheFlavor2 <2500364530@qq.com>
2018-02-20 02:56:15 +0800

    article crawler initial....

---
+++ articleCrawler/main.py
@@ -0,0 +1,7 @@
+from scrapy.cmdline import execute
+import sys
+import os
+
+# set current path = project root directory
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(["scrapy", "crawl", "jobbole"])  # scrapy crawl jobbole
