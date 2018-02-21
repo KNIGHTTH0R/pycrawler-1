a5bb0534a2176ba85c6202d883b98d338b137de8
SavorTheFlavor2 <2500364530@qq.com>
2018-02-21 18:23:09 +0800

    articleSpider parse --> item --> pipelines -->...

--- articleCrawler/articleCrawler/pipelines.py
+++ articleCrawler/articleCrawler/pipelines.py
@@ -1,11 +1,55 @@
 # -*- coding: utf-8 -*-
 
 # Define your item pipelines here
-#
+# item will flow through every pipeline setting in the settings.py
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.pipelines.images import ImagesPipeline
+import codecs
+from articleCrawler.utils.common import get_project_dir
+import json
+from scrapy.exporters import JsonItemExporter
 
 
 class ArticlecrawlerPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class JsonWithEncodingPipeline(object):
+    # 自定义的json导出
+    def __init__(self):
+        self.file = codecs.open(get_project_dir() + '\\json\\article.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"  # 并不确保是ascii
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JsonExporterPipeline(object):
+    # 使用scrapy提供的json导出... 还有CsvItemExporter, XmlItemExporter....
+    def __init__(self):
+        self.file = codecs.open(get_project_dir() + '\\json\\article_exporter.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding="utf-8", ensure_ascii=False)
+        self.exporter.start_exporting()  # self.file.write(b"[")
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()  # self.file.write(b"]")
+        self.file.close()
+
+
+class ArticleImagePipeline(ImagesPipeline):
+    # results : [(ok, dict),....]
+    def item_completed(self, results, item, info):
+        for ok, value in results:
+            image_file_path = value["path"]
+            item["front_image_url_path"] = image_file_path
         return item
--- articleCrawler/articleCrawler/spiders/jobbole.py
+++ articleCrawler/articleCrawler/spiders/jobbole.py
@@ -3,6 +3,8 @@ import scrapy
 import re
 from urllib import parse
 from scrapy import Request
+from articleCrawler.items import JobBoleArticleItem
+from articleCrawler.utils.common import get_md5
 
 
 class JobboleSpider(scrapy.Spider):
@@ -11,17 +13,19 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        # get all url and use self.parse_detail to parse it
-        posts_url = response.css('#archive div.floated-thumb .post-thumb a::attr("href")').extract()
-        # post_url may not have base url, so use urljoin to complete it
-        for post_url in posts_url:
-            print("crawling url:{}........", post_url)
+        post_nodes = response.css('#archive div.floated-thumb .post-thumb a')
+        for post_node in post_nodes:
+            # url may not have base url(domain), so use urljoin to complete it; 没有则使用response.url补全，有则忽视
+            img_url = parse.urljoin(response.url, post_node.css('img::attr(src)').extract_first(""))
+            post_url = parse.urljoin(response.url, post_node.css('::attr(href)').extract_first(""))
+            print("crawling url={}........".format(post_url))
             # 每次调用在此处返回一个Request对象，下次调用再返回下一个Request对象，像iterator一样的生成器函数
-            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+            # 通过meta写入response传递参数到parse_detail
+            yield Request(url= post_url, meta={"front_image_url":img_url}, callback=self.parse_detail)
         # next page  class="next page-numbers"
-        next_page = response.css('.next.page-numbers::attr(href)').extract_first("")
-        if next_page:
-            yield Request(url=parse.urljoin(response.url, next_page), callback=self.parse)
+        # next_page = response.css('.next.page-numbers::attr(href)').extract_first("")
+        # if next_page:
+        #     yield Request(url=parse.urljoin(response.url, next_page), callback=self.parse)
 
 
     def parse_detail(self, response):
@@ -34,6 +38,8 @@ class JobboleSpider(scrapy.Spider):
         title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
         # title = response.css(".entry-header h1::text")[0].extract()
         # title = response.xpath('//div[@class="entry-header"]').xpath('//h1/text()').extract()[0]
+
+        front_image_url = response.meta.get("front_image_url", "")# 文章封面图(点进去之前的缩略图..)
 
         create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()')[0].extract()
         create_date = create_date.strip().replace("·", " ").strip()
@@ -64,4 +70,17 @@ class JobboleSpider(scrapy.Spider):
         tag_list = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
         tag_list = [e for e in tag_list if not e.strip().endswith('评论')]
         tag_list = ','.join(tag_list)
+
+        article_item = JobBoleArticleItem()
+        article_item["title"] = title
+        article_item["url"] = response.url
+        article_item["url_object_id"] = get_md5(response.url)
+        article_item["front_image_url"] = [front_image_url]
+        article_item["create_date"] = create_date
+        article_item["vote_up_nums"] = vote_up_nums
+        article_item["comment_nums"] = comment_nums
+        article_item["tags"] = tag_list
+        article_item["content"] = content
+
+        yield article_item # deliver this item to pipelines
 
