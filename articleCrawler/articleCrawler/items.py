8eeb8deb0ad44777f25028a1fc13b837fbd24fad
SavorTheFlavor <2500364530@qq.com>
2018-02-27 13:02:41 +0800

    lagou -- custom_settings中添加headers信息
    lagou会检查headers参数和cookie信息，不是符合要求的请求会被重定向

--- articleCrawler/articleCrawler/items.py
+++ articleCrawler/articleCrawler/items.py
@@ -151,3 +151,20 @@ class ZhihuAnswerItem(scrapy.Item):
             self["crawl_time"].strftime(SQL_DATETIME_FORMAT),
         )
         return insert_sql, params
+
+class LagouJobItem(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
+    salary = scrapy.Field()
+    job_city = scrapy.Field()
+    work_years = scrapy.Field()
+    degree_need = scrapy.Field()
+    job_type = scrapy.Field()
+    publish_time = scrapy.Field()
+    job_advantage = scrapy.Field()
+    job_desc = scrapy.Field()
+    job_addr = scrapy.Field()
+    company_name = scrapy.Field()
+    company_url = scrapy.Field()
+    crawl_time = scrapy.Field()
+    crawl_update_time = scrapy.Field()
